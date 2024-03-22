5aa47d5a776bc0f590fe62579ee57b98fe985a48
Jacob L. <lecoqjacob@gmail.com>
2024-03-22 09:47:25 -0500

    chore: add bash + fix nvim

--- dot_config/nvim/lua/plugins/user.lua
+++ dot_config/nvim/lua/plugins/user.lua
@@ -1,135 +1,137 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
-	callback = function()
-		vim.schedule(require("chezmoi.commands.__edit").watch)
-	end,
+  pattern = { os.getenv "HOME" .. "/.local/share/chezmoi/*" },
+  callback = function() vim.schedule(require("chezmoi.commands.__edit").watch) end,
 })
 
 vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
-	desc = "Toggle Spectre",
+  desc = "Toggle Spectre",
 })
 vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-	desc = "Search current word",
+  desc = "Search current word",
 })
 vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-	desc = "Search current word",
+  desc = "Search current word",
 })
 vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-	desc = "Search on current file",
+  desc = "Search on current file",
 })
 
 --@type LazySpec
 return {
 
-	-- == Examples of Adding Plugins ==
+  -- == Examples of Adding Plugins ==
 
-	"andweeb/presence.nvim",
-	{
-		"ray-x/lsp_signature.nvim",
-		event = "BufRead",
-		config = function()
-			require("lsp_signature").setup()
-		end,
-	},
+  "andweeb/presence.nvim",
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
 
-	-- == Examples of Overriding Plugins ==
+  -- == Examples of Overriding Plugins ==
 
-	-- customize alpha options
-	{
-		"goolord/alpha-nvim",
-		opts = function(_, opts)
-			-- customize the dashboard header
-			opts.section.header.val = {
-				" █████  ███████ ████████ ██████   ██████",
-				"██   ██ ██         ██    ██   ██ ██    ██",
-				"███████ ███████    ██    ██████  ██    ██",
-				"██   ██      ██    ██    ██   ██ ██    ██",
-				"██   ██ ███████    ██    ██   ██  ██████",
-				" ",
-				"    ███    ██ ██    ██ ██ ███    ███",
-				"    ████   ██ ██    ██ ██ ████  ████",
-				"    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-				"    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-				"    ██   ████   ████   ██ ██      ██",
-			}
-			return opts
-		end,
-	},
+  -- customize alpha options
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+        " █████  ███████ ████████ ██████   ██████",
+        "██   ██ ██         ██    ██   ██ ██    ██",
+        "███████ ███████    ██    ██████  ██    ██",
+        "██   ██      ██    ██    ██   ██ ██    ██",
+        "██   ██ ███████    ██    ██   ██  ██████",
+        " ",
+        "    ███    ██ ██    ██ ██ ███    ███",
+        "    ████   ██ ██    ██ ██ ████  ████",
+        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+        "    ██   ████   ████   ██ ██      ██",
+      }
+      return opts
+    end,
+  },
 
-	-- You can disable default plugins as follows:
-	{ "max397574/better-escape.nvim", enabled = false },
+  -- You can disable default plugins as follows:
+  { "max397574/better-escape.nvim", enabled = false },
 
-	-- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-	{
-		"L3MON4D3/LuaSnip",
-		config = function(plugin, opts)
-			require("astronvim.plugins.configs.luasnip")(plugin, opts) -- include the default astronvim config that calls the setup call
-			-- add more custom luasnip configuration such as filetype extend or custom snippets
-			local luasnip = require("luasnip")
-			luasnip.filetype_extend("javascript", { "javascriptreact" })
-		end,
-	},
+  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+    end,
+  },
 
-	{
-		"windwp/nvim-autopairs",
-		config = function(plugin, opts)
-			require("astronvim.plugins.configs.nvim-autopairs")(plugin, opts) -- include the default astronvim config that calls the setup call
-			-- add more custom autopairs configuration such as custom rules
-			local npairs = require("nvim-autopairs")
-			local Rule = require("nvim-autopairs.rule")
-			local cond = require("nvim-autopairs.conds")
-			npairs.add_rules(
-				{
-					Rule("$", "$", { "tex", "latex" })
-						-- don't add a pair if the next character is %
-						:with_pair(cond.not_after_regex("%%"))
-						-- don't add a pair if  the previous character is xxx
-						:with_pair(
-							cond.not_before_regex("xxx", 3)
-						)
-						-- don't move right when repeat character
-						:with_move(cond.none())
-						-- don't delete if the next character is xx
-						:with_del(cond.not_after_regex("xx"))
-						-- disable adding a newline when you press <cr>
-						:with_cr(cond.none()),
-				},
-				-- disable for .vim files, but it work for another filetypes
-				Rule("a", "a", "-vim")
-			)
-		end,
-	},
+  {
+    "windwp/nvim-autopairs",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom autopairs configuration such as custom rules
+      local npairs = require "nvim-autopairs"
+      local Rule = require "nvim-autopairs.rule"
+      local cond = require "nvim-autopairs.conds"
+      npairs.add_rules(
+        {
+          Rule("$", "$", { "tex", "latex" })
+            -- don't add a pair if the next character is %
+            :with_pair(cond.not_after_regex "%%")
+            -- don't add a pair if  the previous character is xxx
+            :with_pair(
+              cond.not_before_regex("xxx", 3)
+            )
+            -- don't move right when repeat character
+            :with_move(cond.none())
+            -- don't delete if the next character is xx
+            :with_del(cond.not_after_regex "xx")
+            -- disable adding a newline when you press <cr>
+            :with_cr(cond.none()),
+        },
+        -- disable for .vim files, but it work for another filetypes
+        Rule("a", "a", "-vim")
+      )
+    end,
+  },
 
-	-- search/replace in multiple files
-	{
-		"nvim-pack/nvim-spectre",
-		build = false,
-		cmd = "Spectre",
-		opts = { open_cmd = "noswapfile vnew" },
+  -- search/replace in multiple files
+  {
+    "nvim-pack/nvim-spectre",
+    build = false,
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
 		-- stylua: ignore
 		keys = {
 			{ "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
 		},
-	},
+  },
 
-	{
-		"xvzc/chezmoi.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("chezmoi").setup({
-				edit = {
-					watch = true,
-					force = false,
-				},
-				notification = {
-					on_open = true,
-					on_apply = true,
-					on_watch = true,
-				},
-				telescope = {
-					select = { "<CR>" },
-				},
-			})
-		end,
-	},
+  {
+    "xvzc/chezmoi.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("chezmoi").setup {
+        edit = {
+          watch = true,
+          force = false,
+        },
+        notification = {
+          on_open = true,
+          on_apply = true,
+          on_watch = true,
+        },
+        telescope = {
+          select = { "<CR>" },
+        },
+      }
+    end,
+  },
+
+  {
+    "sindrets/diffview.nvim",
+    opts = {},
+    config = true,
+  },
 }
